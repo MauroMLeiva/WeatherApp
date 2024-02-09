4854d30c9b7f4f540761120b310d1e913b5f7a41
Mauro Leiva <leivam.mauro@gmail.com>
2024-02-09 00:57:51 -0300

    Modified Current Weather component layout and style

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,56 +1,50 @@
 export const CurrentWeather = () => {
     return (
         <div className='currentWeather container text-center'>
-            <div className='row'>
-                <div className='col'>
-                    <p className='title'>Córdoba, Ar</p>
+            <div className='row gy-2 align-items-center'>
+                <div className='col-12'>
+                    <span className='title'>Córdoba, Ar</span>
                 </div>
-            </div>
 
-            <div className='row align-items-center'>
-                <div className='col'>
+                <div className='col-6'>
                     <img
                         alt='weather icon'
                         className='weather-icon'
                         src='01d.png'
                     />
                 </div>
-                <div className='col'>
+                <div className='col-6 p-3'>
                     <p className='data-1'>26°C</p>
                     <p className='data-2'>Cloudy</p>
                 </div>
-            </div>
 
-            <div className='row'>
-                <div className='col'>
-                    <p className='subtitle'>Max / Min</p>
+                <div className='col-4'>
+                    <span className='subtitle'>Max / Min</span>
                     <p className='data-3'>30 / 24</p>
                 </div>
 
-                <div className='col'>
-                    <p className='subtitle'>Feels like</p>
+                <div className='col-4'>
+                    <span className='subtitle'>Feels like</span>
                     <p className='data-3'>29°C</p>
                 </div>
 
-                <div className='col'>
-                    <p className='subtitle'>Humidity</p>
+                <div className='col-4'>
+                    <span className='subtitle'>Humidity</span>
                     <p className='data-3'>96%</p>
                 </div>
-            </div>
 
-            <div className='row'>
-                <div className='col'>
-                    <p className='subtitle'>Pressure</p>
+                <div className='col-4'>
+                    <span className='subtitle'>Pressure</span>
                     <p className='data-3'>1023</p>
                 </div>
 
-                <div className='col'>
-                    <p className='subtitle'>Wind</p>
-                    <p className='data-3'>20 mts/sec 20°</p>
+                <div className='col-4'>
+                    <span className='subtitle'>Wind</span>
+                    <p className='data-3'>20 mt/s</p>
                 </div>
 
-                <div className='col'>
-                    <p className='subtitle'>Visibility</p>
+                <div className='col-4'>
+                    <span className='subtitle'>Visibility</span>
                     <p className='data-3'>5 km</p>
                 </div>
             </div>
