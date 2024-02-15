b1eada2947465e2ff3d04368cf3d83b044506c8b
Mauro Leiva <leivam.mauro@gmail.com>
2024-02-14 23:00:00 -0300

    Changed styles and information display.

--- src/components/FavoriteItem.jsx
+++ src/components/FavoriteItem.jsx
@@ -22,18 +22,22 @@ export const FavoriteItem = ({ coords, setFavs }) => {
     };
 
     return (
-        <div className='container favorite-item center'>
-            <button
-                onClick={handleRemoveFavorite}
-                type='button'
-                className='btn btn-outline-dark btn-lg'
-                title='Favorite'
-            >
-                <i className='bi-star-fill'></i>
-            </button>
-            <span className='title'>{data?.name}</span>
-            <img alt='weather icon' src={`${data?.weather[0].icon}.png`} />
-            <span className='data-2'>{data?.main.temp}°</span>
+        <div className='row container favorite-item'>
+            <div className='col-auto favorite-item-btn'>
+                <button
+                    onClick={handleRemoveFavorite}
+                    type='button'
+                    className='btn btn-outline-dark btn-lg'
+                    title='Favorite'
+                >
+                    <i className='bi-star-fill'></i>
+                </button>
+            </div>
+            <div className='col center'>
+                <span className='title'>{data?.name}</span>
+                <img alt='weather icon' src={`${data?.weather[0].icon}.png`} />
+                <span className='data-2'>{data?.main.temp.toFixed(1)}°</span>
+            </div>
         </div>
     );
 };
